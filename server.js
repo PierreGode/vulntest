c56cd34f28253ebd73fcdb68bcb329f15edb944e
PierreGode <pierre@gode.one>
2026-02-08 23:19:38 +0100

    Change server port from 3000 to 3009

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3009;
 const expectedToken = process.env.BEARER_TOKEN || 'scanner-test-token';
 
 app.use(express.json());
